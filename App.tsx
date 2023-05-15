ca99e2f8d9e980931937e8eae9c11be1890b018c
unknown <borges10002@gmail.com>
2023-05-14 21:45:52 -0400

    class statusBar

--- App.tsx
+++ App.tsx
@@ -1,23 +1,29 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import { StatusBar } from "expo-status-bar";
 
-import Teste from "@screens/Teste";
+import { DMSans_400Regular, useFonts } from "@expo-google-fonts/dm-sans";
+import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
+
+import AppLoading from "expo-app-loading";
+
+import theme from "./src/theme";
+import { SignIn } from "./src/screens/SignIn";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    DMSans_400Regular,
+    DMSerifDisplay_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style='auto' />
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar style='light' translucent backgroundColor='transparent' />
+      <SignIn />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
